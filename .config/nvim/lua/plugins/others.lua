cc7e6cd2204c0dc69e993076ab9b2a30ee6ac065
Andrés Yagüe López <and.yague@gmail.com>
2025-07-20 12:53:02 -0600

    Improved lualine configuration

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -48,7 +48,72 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {},
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+          refresh_time = 16, -- ~60fps
+          events = {
+            'WinEnter',
+            'BufEnter',
+            'BufWritePost',
+            'SessionLoadPost',
+            'FileChangedShellPost',
+            'VimResized',
+            'Filetype',
+            'CursorMoved',
+            'CursorMovedI',
+            'ModeChanged',
+          },
+        },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          'branch',
+          'diff',
+          { 'lsp_status', icon = '' },
+          'diagnostics',
+        },
+        lualine_c = {
+          'filename',
+          'searchcount',
+        },
+        lualine_x = {
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+        lualine_y = { 'selectioncount', 'progress', 'location' },
+        lualine_z = { { 'datetime', style = '%a %d-%m-%y %H:%M' } },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
+    },
   },
   {
     'akinsho/bufferline.nvim',
