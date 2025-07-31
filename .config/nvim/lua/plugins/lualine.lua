c82d26400752ed784274b3de7da091ffab97278b
Andrés Yagüe López <and.yague@gmail.com>
2025-07-31 07:16:17 -0600

    Added more modes to lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -36,7 +36,58 @@ return {
         },
       },
       sections = {
-        lualine_a = { 'mode' },
+        lualine_a = {
+          {
+            -- Add X mode to "mode",
+            function()
+              -- Dictionary with modes
+              local mode_names = {
+                n = 'NORMAL',
+                no = 'O-PENDING',
+                nov = 'O-PENDING',
+                noV = 'O-PENDING',
+                niI = 'I-NORMAL',
+                niR = 'R-NORMAL',
+                niV = 'V-NORMAL',
+                nt = 'NORMAL',
+                ntT = 'I-NORMAL',
+                v = 'VISUAL',
+                vs = 'S-VISUAL',
+                V = 'VISUAL',
+                Vs = 'S-VISUAL',
+                s = 'SELECT',
+                S = 'SELECT',
+                i = 'INSERT',
+                ic = 'I-COMPLETION',
+                ix = 'X-MODE',
+                R = 'REPLACE',
+                Rc = 'R-COMPLETION',
+                Rx = 'X-MODE',
+                Rv = 'VIRT-REPLACE',
+                Rvc = 'VR-COMPLETION',
+                Rvx = 'X-MODE',
+                c = 'COMMAND',
+                cr = 'OVERSTRIKE',
+                cv = 'EX',
+                cvr = 'EX-OVERSTRIKE',
+                r = 'PROMPT',
+                rm = 'MORE',
+                t = 'TERMINAL',
+              }
+              -- Add the modes that have special characters
+              mode_names['noCTRL-V'] = 'O-PENDING'
+              mode_names['CTRL-V'] = 'VISUAL'
+              mode_names['CTRL-Vs'] = 'VISUAL'
+              mode_names['CTRL-S'] = 'SELECT'
+              mode_names['r?'] = 'CONFIRM'
+              mode_names['!'] = 'EXTERNAL'
+
+              -- Retrieve and return mode
+              return mode_names[vim.api.nvim_get_mode().mode]
+
+            end,
+          },
+        },
         lualine_b = {
           'branch',
           'diff',
