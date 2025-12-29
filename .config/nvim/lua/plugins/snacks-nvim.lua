1ccf94a3968fd5eb6d37b2a27b28763dcd105809
Andrés Yagüe López <and.yague@gmail.com>
2025-12-28 18:33:20 -0700

    Fixed the snacks keybinds

--- .config/nvim/lua/plugins/snacks-nvim.lua
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -48,14 +48,14 @@ Snacks.setup {
     },
     sections = {
       { section = 'header' },
-      { section = 'keys', gap = 1, padding = 1 },
+      { section = 'keys',  gap = 1, padding = 1 },
     },
-  }, -- Initial neovim dashboard
-  explorer = { enabled = true }, -- File explorer
-  image = { enabled = true }, -- Render images
+  },                                                       -- Initial neovim dashboard
+  explorer = { enabled = true },                           -- File explorer
+  image = { enabled = true },                              -- Render images
   indent = { enabled = true, chunk = { enabled = true } }, -- Indent lines
-  quickfile = { enabled = true }, -- Load file as fast as possible
-  rename = { enabled = true }, -- Rename files
+  quickfile = { enabled = true },                          -- Load file as fast as possible
+  rename = { enabled = true },                             -- Rename files
   scope = {
     enabled = true,
     treesitter = {
@@ -79,15 +79,15 @@ Snacks.setup {
         'local_declaration',
       },
     },
-  }, -- Scope jumps
-  lazygit = { enabled = true }, -- Lazygit
-  notifier = { enabled = true }, -- Better notifications
-  scratch = { enabled = true }, -- Scratch space
-  scroll = { enabled = true }, -- Smooth scrolling
+  },                                 -- Scope jumps
+  lazygit = { enabled = true },      -- Lazygit
+  notifier = { enabled = true },     -- Better notifications
+  scratch = { enabled = true },      -- Scratch space
+  scroll = { enabled = true },       -- Smooth scrolling
   statuscolumn = { enabled = true }, -- Status column on its own
-  toggle = { enabled = true }, -- Toggle things
-  words = { enabled = true }, -- LSP help for references
-  zen = { enabled = true }, -- Zen/Zoom mode
+  toggle = { enabled = true },       -- Toggle things
+  words = { enabled = true },        -- LSP help for references
+  zen = { enabled = true },          -- Zen/Zoom mode
   picker = {
     enabled = true,
     win = {
@@ -111,6 +111,7 @@ Snacks.setup {
           local list = vim.api.nvim_cmd({
             cmd = '!',
             args = { 'rg --type-list' },
+            ---@diagnostic disable-next-line: missing-fields
             mods = { silent = true },
           }, { output = true })
 
@@ -131,12 +132,13 @@ Snacks.setup {
           function(choice)
             -- User canceled
             if not choice then
-              return true
+              return
             end
 
             -- If the choice is all, handle it properly
             if choice == 'all' then
               picker.opts.prompt = ' '
+              ---@diagnostic disable-next-line: inject-field
               picker.opts.ft = {}
             else
               local icon = devicons.get_icon_by_filetype(choice, {})
@@ -146,77 +148,12 @@ Snacks.setup {
               else
                 picker.opts.prompt = choice .. ' '
               end
+              ---@diagnostic disable-next-line: inject-field
               picker.opts.ft = choice
             end
           end
         )
       end,
-    },
-  },
-  keys = {
-    {
-      '<C-w>z',
-      function()
-        Snacks.zen()
-      end,
-      desc = 'Toggle Zen Mode',
-    },
-    {
-      '<C-w>m',
-      function()
-        Snacks.zen.zoom()
-      end,
-      desc = 'Toggle Zoom',
-    },
-    {
-      '<leader>.',
-      function()
-        Snacks.scratch()
-      end,
-      desc = 'Toggle Scratch Buffer',
-    },
-    {
-      '<leader>S',
-      function()
-        Snacks.scratch.select()
-      end,
-      desc = 'Select Scratch Buffer',
-    },
-    {
-      '<leader>n',
-      function()
-        Snacks.notifier.show_history()
-      end,
-      desc = 'Notification History',
-    },
-    {
-      '<leader>cR',
-      function()
-        Snacks.rename.rename_file()
-      end,
-      desc = 'Rename File',
-    },
-    {
-      '<leader>gB',
-      function()
-        Snacks.gitbrowse()
-      end,
-      desc = 'Git Browse',
-      mode = { 'n', 'v' },
-    },
-    {
-      '<leader>gg',
-      function()
-        Snacks.lazygit()
-      end,
-      desc = 'Lazygit',
-    },
-    {
-      '<leader>un',
-      function()
-        Snacks.notifier.hide()
-      end,
-      desc = 'Dismiss All Notifications',
     },
   },
 }
@@ -238,111 +175,139 @@ vim.api.nvim_create_autocmd('User', {
     Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
     Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
     Snacks.toggle
-      .option('relativenumber', { name = 'Relative Number' })
-      :map '<leader>uL'
+        .option('relativenumber', { name = 'Relative Number' })
+        :map '<leader>uL'
     Snacks.toggle.diagnostics():map '<leader>ud'
     Snacks.toggle.line_number():map '<leader>ul'
     Snacks.toggle
-      .option('conceallevel', {
-        off = 0,
-        on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
-      })
-      :map '<leader>uc'
+        .option('conceallevel', {
+          off = 0,
+          on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2,
+        })
+        :map '<leader>uc'
     Snacks.toggle.treesitter():map '<leader>uT'
     Snacks.toggle.inlay_hints():map '<leader>uh'
     Snacks.toggle.indent():map '<leader>ug'
     Snacks.toggle.dim():map '<leader>uD'
     Snacks.toggle.animate():map '<leader>ua'
     Snacks.toggle
-      .option('cursorline', { name = 'Cursor Line' })
-      :map '<leader>uR'
+        .option('cursorline', { name = 'Cursor Line' })
+        :map '<leader>uR'
     Snacks.toggle
-      .option('cursorcolumn', { name = 'Cursor Column' })
-      :map '<leader>ur'
+        .option('cursorcolumn', { name = 'Cursor Column' })
+        :map '<leader>ur'
 
     -- Toggle context
     Snacks.toggle
-      .new({
-        id = 'treesitter-context',
-        name = 'Treesitter Context',
-        get = function()
-          return require('treesitter-context').enabled()
-        end,
-        set = function()
-          require('treesitter-context').toggle()
-        end,
-      })
-      :map '<leader>ut'
+        .new({
+          id = 'treesitter-context',
+          name = 'Treesitter Context',
+          get = function()
+            return require('treesitter-context').enabled()
+          end,
+          set = function()
+            require('treesitter-context').toggle()
+          end,
+        })
+        :map '<leader>ut'
 
     -- Toggle autopairs
     Snacks.toggle
-      .new({
-        id = 'nvim-autopairs',
-        name = 'Autopairs',
-        get = function()
-          return not require('nvim-autopairs').state.disabled
-        end,
-        set = function()
-          require('nvim-autopairs').toggle()
-        end,
-      })
-      :map '<leader>up'
+        .new({
+          id = 'nvim-autopairs',
+          name = 'Autopairs',
+          get = function()
+            return not require('nvim-autopairs').state.disabled
+          end,
+          set = function()
+            require('nvim-autopairs').toggle()
+          end,
+        })
+        :map '<leader>up'
 
     -- Toggle Markview
     Snacks.toggle
-      .new({
-        id = 'markview',
-        name = 'Markview',
-        get = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          if not require('markview.state').buf_attached(buffer) then
-            require('markview.commands').attach(buffer)
-            require('markview.commands').disable(buffer)
-          end
-          return require('markview.state').get_buffer_state(buffer, false).enable
-        end,
-        set = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          require('markview.commands').toggle(buffer)
-        end,
-      })
-      :map '<leader>um'
+        .new({
+          id = 'markview',
+          name = 'Markview',
+          get = function()
+            local buffer = vim.api.nvim_get_current_buf()
+            if not require('markview.state').buf_attached(buffer) then
+              require('markview.commands').attach(buffer)
+              require('markview.commands').disable(buffer)
+            end
+            return require('markview.state').get_buffer_state(buffer, false).enable
+          end,
+          set = function()
+            local buffer = vim.api.nvim_get_current_buf()
+            require('markview.commands').toggle(buffer)
+          end,
+        })
+        :map '<leader>um'
 
     -- Fully custom toggle for colorizer
     Snacks.toggle
-      .new({
-        id = 'colorizer',
-        name = 'Colorizer',
-        get = function()
-          return require('colorizer').is_buffer_attached(0)
-        end,
-        set = function()
-          vim.cmd 'ColorizerToggle'
-        end,
-      })
-      :map '<leader>uz'
+        .new({
+          id = 'colorizer',
+          name = 'Colorizer',
+          get = function()
+            return require('colorizer').is_buffer_attached(0)
+          end,
+          set = function()
+            vim.cmd 'ColorizerToggle'
+          end,
+        })
+        :map '<leader>uz'
 
     -- Fully custom toggle for visual_bold
     Snacks.toggle
-      .new({
-        id = 'visual_bold',
-        name = 'Visual Bold',
-        get = function()
-          return visual_bold
-        end,
-        set = function(state)
-          if state then
-            vim.cmd.highlight {
-              args = { 'link Visual IncSearch' },
-              bang = true,
-            }
-            visual_bold = true
-          else
-            vim.cmd.highlight { args = { 'link Visual NONE' } }
-            visual_bold = false
-          end
-        end,
-      })
-      :map '<leader>uv'
+        .new({
+          id = 'visual_bold',
+          name = 'Visual Bold',
+          get = function()
+            return visual_bold
+          end,
+          set = function(state)
+            if state then
+              vim.cmd.highlight {
+                args = { 'link Visual IncSearch' },
+                bang = true,
+              }
+              visual_bold = true
+            else
+              vim.cmd.highlight { args = { 'link Visual NONE' } }
+              visual_bold = false
+            end
+          end,
+        })
+        :map '<leader>uv'
   end,
 })
+
+vim.keymap.set('n', '<C-w>z', function()
+  Snacks.zen()
+end, { desc = 'Toggle Zen Mode' })
+vim.keymap.set('n', '<C-w>m', function()
+  Snacks.zen.zoom()
+end, { desc = 'Toggle Zoom' })
+vim.keymap.set('n', '<leader>.', function()
+  Snacks.scratch()
+end, { desc = 'Toggle Scratch Buffer' })
+vim.keymap.set('n', '<leader>S', function()
+  Snacks.scratch.select()
+end, { desc = 'Select Scratch Buffer' })
+vim.keymap.set('n', '<leader>n', function()
+  Snacks.notifier.show_history()
+end, { desc = 'Notification History' })
+vim.keymap.set('n', '<leader>cR', function()
+  Snacks.rename.rename_file()
+end, { desc = 'Rename File' })
+vim.keymap.set({ 'n', 'v' }, '<leader>gB', function()
+  Snacks.gitbrowse()
+end, { desc = 'Git Browse' })
+vim.keymap.set('n', '<leader>gg', function()
+  Snacks.lazygit()
+end, { desc = 'Lazygit' })
+vim.keymap.set('n', '<leader>un', function()
+  Snacks.notifier.hide()
+end, { desc = 'Dismiss All Notifications' })
