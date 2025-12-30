8601c923a81c53ae7c9dafd32ef49ff0759b16d4
Andrés Yagüe López <and.yague@gmail.com>
2025-12-29 18:14:26 -0700

    If renaming a file with Oil, let the LSP know
    This uses snacks.rename

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -30,3 +30,17 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
     end
   end,
 })
+
+-- If renaming a file with Oil, let the LSP know
+-- through snacks.rename
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'OilActionsPost',
+  callback = function(event)
+    if event.data.actions[1].type == 'move' then
+      require('snacks').rename.on_rename_file(
+        event.data.actions[1].src_url,
+        event.data.actions[1].dest_url
+      )
+    end
+  end,
+})
