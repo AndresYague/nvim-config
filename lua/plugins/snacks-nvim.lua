4334fe460459034df60ed2bdf40fa7721e1fe8b8
Andrés Yagüe López <and.yague@gmail.com>
2026-01-02 09:56:44 -0700

    Better blocks and block jumps

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -4,67 +4,81 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    bigfile = { enabled = true },                            -- Deactivates things for files too large
-    dashboard = { enabled = true },                          -- Initial neovim dashboard
-    explorer = { enabled = true },                           -- File explorer
-    image = { enabled = true },                              -- Render images
+    bigfile = { enabled = true }, -- Deactivates things for files too large
+    dashboard = { enabled = true }, -- Initial neovim dashboard
+    explorer = { enabled = true }, -- File explorer
+    image = { enabled = true }, -- Render images
     indent = { enabled = true, chunk = { enabled = true } }, -- Indent lines
-    quickfile = { enabled = true },                          -- Load file as fast as possible
-    rename = { enabled = true },                             -- Rename files
+    quickfile = { enabled = true }, -- Load file as fast as possible
+    rename = { enabled = true }, -- Rename files
     scope = {
       enabled = true,
       treesitter = {
         blocks = {
           enabled = true,
+          'function_declaration',
+          'function_definition',
+          'method_declaration',
+          'method_definition',
+          'class_declaration',
+          'class_definition',
+          'do_statement',
+          'while_statement',
+          'repeat_statement',
+          'if_statement',
+          'for_statement',
+        },
+        -- these treesitter fields will be considered as blocks
+        field_blocks = {
+          'argument_list',
           'arguments',
           'case_statement',
           'catch_clause',
-          'class_declaration',
-          'class_definition',
           'class_specifier',
           'comment',
-          'do_statement',
+          'dictionary',
+          'dictionary_comprehension',
           'enum_specifier',
           'for_range_loop',
-          'for_statement',
-          'function_declaration',
-          'function_definition',
+          'generator_expression',
           'gnu_asm_expression',
-          'if_statement',
+          'import_from_statement',
           'initializer_list',
           'lambda_expression',
-          'method_declaration',
-          'method_definition',
+          'list',
+          'list_comprehension',
+          'local_declaration',
+          'match_statement',
           'namespace_definition',
           'parameters',
+          'parenthesized_expression',
           'preproc_elif',
           'preproc_else',
           'preproc_function_def',
           'preproc_if',
           'preproc_ifdef',
           'preproc_include',
-          'repeat_statement',
+          'set',
+          'set_comprehension',
+          'string',
           'struct_specifier',
           'switch_statement',
           'table_constructor',
           'template_declaration',
           'try_statement',
-          'while_statement',
-        },
-        -- these treesitter fields will be considered as blocks
-        field_blocks = {
-          'local_declaration',
+          'tuple',
+          'with_statement',
         },
       },
-    },                                 -- Scope jumps
-    lazygit = { enabled = true },      -- Lazygit
-    notifier = { enabled = true },     -- Better notifications
-    scratch = { enabled = true },      -- Scratch space
-    scroll = { enabled = true },       -- Smooth scrolling
+    }, -- Scope jumps
+    lazygit = { enabled = true }, -- Lazygit
+    notifier = { enabled = true }, -- Better notifications
+    scratch = { enabled = true }, -- Scratch space
+    scroll = { enabled = true }, -- Smooth scrolling
     statuscolumn = { enabled = true }, -- Status column on its own
-    toggle = { enabled = true },       -- Toggle things
-    words = { enabled = true },        -- LSP help for references
-    zen = { enabled = true },          -- Zen/Zoom mode
+    toggle = { enabled = true }, -- Toggle things
+    words = { enabled = true }, -- LSP help for references
+    zen = { enabled = true }, -- Zen/Zoom mode
   },
   keys = {
     {
@@ -152,112 +166,112 @@ return {
         Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
         Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
         Snacks.toggle
-            .option('relativenumber', { name = 'Relative Number' })
-            :map '<leader>uL'
+          .option('relativenumber', { name = 'Relative Number' })
+          :map '<leader>uL'
         Snacks.toggle.diagnostics():map '<leader>ud'
         Snacks.toggle.line_number():map '<leader>ul'
         Snacks.toggle
-            .option(
-              'conceallevel',
-              { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }
-            )
-            :map '<leader>uc'
+          .option(
+            'conceallevel',
+            { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }
+          )
+          :map '<leader>uc'
         Snacks.toggle.treesitter():map '<leader>uT'
         Snacks.toggle.inlay_hints():map '<leader>uh'
         Snacks.toggle.indent():map '<leader>ug'
         Snacks.toggle.dim():map '<leader>uD'
         Snacks.toggle.animate():map '<leader>ua'
         Snacks.toggle
-            .option('cursorline', { name = 'Cursor Line' })
-            :map '<leader>uR'
+          .option('cursorline', { name = 'Cursor Line' })
+          :map '<leader>uR'
         Snacks.toggle
-            .option('cursorcolumn', { name = 'Cursor Column' })
-            :map '<leader>ur'
+          .option('cursorcolumn', { name = 'Cursor Column' })
+          :map '<leader>ur'
 
         -- Toggle context
         Snacks.toggle
-            .new({
-              id = 'treesitter-context',
-              name = 'Treesitter Context',
-              get = function()
-                return require('treesitter-context').enabled()
-              end,
-              set = function()
-                require('treesitter-context').toggle()
-              end,
-            })
-            :map '<leader>ut'
+          .new({
+            id = 'treesitter-context',
+            name = 'Treesitter Context',
+            get = function()
+              return require('treesitter-context').enabled()
+            end,
+            set = function()
+              require('treesitter-context').toggle()
+            end,
+          })
+          :map '<leader>ut'
 
         -- Toggle autopairs
         Snacks.toggle
-            .new({
-              id = 'nvim-autopairs',
-              name = 'Autopairs',
-              get = function()
-                return not require('nvim-autopairs').state.disabled
-              end,
-              set = function()
-                require('nvim-autopairs').toggle()
-              end,
-            })
-            :map '<leader>up'
+          .new({
+            id = 'nvim-autopairs',
+            name = 'Autopairs',
+            get = function()
+              return not require('nvim-autopairs').state.disabled
+            end,
+            set = function()
+              require('nvim-autopairs').toggle()
+            end,
+          })
+          :map '<leader>up'
 
         -- Toggle Markview
         Snacks.toggle
-            .new({
-              id = 'markview',
-              name = 'Markview',
-              get = function()
-                local buffer = vim.api.nvim_get_current_buf()
-                if not require('markview.state').buf_attached(buffer) then
-                  require('markview.commands').attach(buffer)
-                  require('markview.commands').disable(buffer)
-                end
-                return require('markview.state').get_buffer_state(buffer, false).enable
-              end,
-              set = function()
-                local buffer = vim.api.nvim_get_current_buf()
-                require('markview.commands').toggle(buffer)
-              end,
-            })
-            :map '<leader>um'
+          .new({
+            id = 'markview',
+            name = 'Markview',
+            get = function()
+              local buffer = vim.api.nvim_get_current_buf()
+              if not require('markview.state').buf_attached(buffer) then
+                require('markview.commands').attach(buffer)
+                require('markview.commands').disable(buffer)
+              end
+              return require('markview.state').get_buffer_state(buffer, false).enable
+            end,
+            set = function()
+              local buffer = vim.api.nvim_get_current_buf()
+              require('markview.commands').toggle(buffer)
+            end,
+          })
+          :map '<leader>um'
 
         -- Fully custom toggle for colorizer
         Snacks.toggle
-            .new({
-              id = 'colorizer',
-              name = 'Colorizer',
-              get = function()
-                return require('colorizer').is_buffer_attached(0)
-              end,
-              set = function()
-                vim.cmd 'ColorizerToggle'
-              end,
-            })
-            :map '<leader>uz'
+          .new({
+            id = 'colorizer',
+            name = 'Colorizer',
+            get = function()
+              return require('colorizer').is_buffer_attached(0)
+            end,
+            set = function()
+              vim.cmd 'ColorizerToggle'
+            end,
+          })
+          :map '<leader>uz'
 
         -- Fully custom toggle for visual_bold
         Snacks.toggle
-            .new({
-              id = 'visual_bold',
-              name = 'Visual Bold',
-              get = function()
-                return visual_bold
-              end,
-              set = function(state)
-                if state then
-                  vim.cmd.highlight {
-                    args = { 'link Visual IncSearch' },
-                    bang = true,
-                  }
-                  visual_bold = true
-                else
-                  vim.cmd.highlight { args = { 'link Visual NONE' } }
-                  visual_bold = false
-                end
-              end,
-            })
-            :map '<leader>uv'
+          .new({
+            id = 'visual_bold',
+            name = 'Visual Bold',
+            get = function()
+              return visual_bold
+            end,
+            set = function(state)
+              if state then
+                vim.cmd.highlight {
+                  args = { 'link Visual IncSearch' },
+                  bang = true,
+                }
+                visual_bold = true
+              else
+                vim.cmd.highlight { args = { 'link Visual NONE' } }
+                visual_bold = false
+              end
+            end,
+          })
+          :map '<leader>uv'
       end,
     })
   end,
