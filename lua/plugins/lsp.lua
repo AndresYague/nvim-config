a54bbfbcb9dde1b2aa83691e10f211c3350e415b
Andrés Yagüe López <and.yague@gmail.com>
2026-05-03 09:15:38 -0600

    Underlining warnings as well

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -106,7 +106,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.diagnostic.config {
   severity_sort = true,
   float = { border = 'rounded', source = 'if_many' },
-  underline = { severity = vim.diagnostic.severity.ERROR },
+  underline = { severity = { min = vim.diagnostic.severity.WARN } },
   signs = vim.g.have_nerd_font and {
     text = {
       [vim.diagnostic.severity.ERROR] = '󰅚 ',
