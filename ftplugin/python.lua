4168b59f9a42e1ba6a693607a5dfcdc7e17db773
Andrés Yagüe López <and.yague@gmail.com>
2026-09-15 20:35:01 -0600

    Use ":append" for "vim.opt" options

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,8 +1,6 @@
 -- Add python format to errorformat
--- FIXME: Does not work too well but it finds something
--- using vim.opt.errorformat:get() produces a table and this way does not work
--- at all.
-vim.o.errorformat = vim.o.errorformat .. ',\\ \\ File "%f"\\, line %l\\, %m'
+-- FIXME: Does not work too well: fix the errorformat
+vim.opt.errorformat:append { '\\ \\ File "%f"\\', 'line %l\\', '%m' }
 
 -- Make python add comments on new line
 vim.bo.formatoptions = 'jcroql'
--- lua/plugins/ui2.lua
+++ lua/plugins/ui2.lua
@@ -2,6 +2,5 @@ require('vim._core.ui2').enable()
 
 -- Configure the extra options in messagesopt
 if vim.fn.has 'nvim-0.13.0' == 1 then
-  vim.o.messagesopt = vim.o.messagesopt
-    .. ',maxheight:50,pager:<CR>,timeout:4000'
+  vim.opt.messagesopt:append { 'maxheight:50', 'pager:<CR>', 'timeout:4000' }
 end
